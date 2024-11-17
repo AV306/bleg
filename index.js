3f5fe40bea9b48715c787ab00ee8451b6d478bbd
Blender <brandonjoash@gmail.com>
2024-11-17 16:52:48 +0800

    hmm

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
+const baseUrl = "https://av306.github.io/bleg"
+
 function prepareLinks()
 {
     // Who needs React? I've got all I need for dynamic content right here!
     // Fill in the href of gallery-element <a>s according to their <h2>'s content
     for ( let link of document.querySelectorAll( "div.gallery-element > a" ) )
-        link.href = `${document.location.href}/${link.firstChild.innerText.toLowerCase()}`.replace( "index.html", '' ).replace( "//", '/' ).replace( ' ', '-' );
+    {
+        console.log( link.firstChild.innerText.toLowerCase() );
+        link.href = `${document.location.pathname}/${link.firstChild.innerText.toLowerCase()}`.replace( "index.html", '' ).replace( "//", '/' ).replace( ' ', '-' );
+    }
 
     for ( let link of document.querySelectorAll( "nav > a" ) )
     {
@@ -16,7 +21,7 @@ function prepareLinks()
             
             case "Blog index":
             {
-                //link.href = document.location.href;
+                link.href = baseUrl;
             } break;
         }
     }
